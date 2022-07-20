1cf15baf8fb48fd0eab66be5b4cf6d9a6cc415fa
HaoyangHu <hu.haoy@northeastern.edu>
2022-07-19 23:11:52 -0400

    portfolio model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import profileImage from './img/bili_img.png'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ProjectDisplay from "./pages/ProjectDisplay";
 
 function App() {
   return (
     <div className="App">
-      <h1>My portfolio change test</h1>
-      <h2>Author: Haoyang Hu</h2>
-      <img src={profileImage} alt="profile" />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
